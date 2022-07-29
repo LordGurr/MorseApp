4945e09720eccb0c2381690b2a0a3ffa45dd1df7
Gustav Juul <50996232+LordGurr@users.noreply.github.com>
2022-07-29 23:02:36 +0200

    la in settings, klick och ljud

--- Morse/Assets/Scripts/Morse.cs
+++ Morse/Assets/Scripts/Morse.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
 
 public class Morse : MonoBehaviour
 {
@@ -173,17 +175,60 @@ public class Morse : MonoBehaviour
 
     [SerializeField] private TextAsset textAsset;
 
+    [SerializeField] private GameObject[] KeyboardObjects;
+
+    [SerializeField] private Slider sliderUnit;
+    [SerializeField] private TMP_InputField inputFieldUnit;
+
+    [SerializeField] private Slider sliderFrequency;
+    [SerializeField] private TMP_InputField inputFieldFrequecny;
+
     // Start is called before the first frame update
     private void Start()
     {
         ord = textAsset.text.Split('\n');
 
-        nuvarandeOrd = ord[Random.Range(0, ord.Length)];
+        nuvarandeOrd = ord[UnityEngine.Random.Range(0, ord.Length)];
         textMesh.text = nuvarandeOrd;
         morse.text = string.Empty;
         nuvarandeOrdMorse = ConvertToMorse(nuvarandeOrd);
         gissadeKorrektBokstav = new bool[nuvarandeOrd.Length];
         SetWordColour();
+
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialBlend = 0; //force 2D sound
+        audioSource.Stop(); //avoids audiosource from starting to play automatically
+
+        sliderUnit.value = unitLength * 1000;
+        inputFieldUnit.text = (unitLength * 1000).ToString();
+
+        sliderFrequency.value = frequency;
+        inputFieldFrequecny.text = (frequency).ToString();
+    }
+
+    public void UpdateSoundFrequencyFromInputfield()
+    {
+        frequency = float.Parse(inputFieldFrequecny.text);
+        sliderFrequency.value = frequency;
+    }
+
+    public void UpdateSoundFrequencyFromSlider()
+    {
+        frequency = sliderFrequency.value;
+        inputFieldFrequecny.text = (frequency).ToString();
+    }
+
+    public void UpdateUnitLengthFromInputfield()
+    {
+        unitLength = float.Parse(inputFieldUnit.text) / 1000;
+        sliderUnit.value = unitLength * 1000;
+    }
+
+    public void UpdateUnitLengthFromSlider()
+    {
+        unitLength = sliderUnit.value / 1000;
+        inputFieldUnit.text = (unitLength * 1000).ToString();
     }
 
     public void DotPressed()
@@ -211,9 +256,9 @@ public class Morse : MonoBehaviour
             SetWordColour();
         }
 
-        if (currentLetter >= nuvarandeOrd.Length)
+        if (currentLetter >= nuvarandeOrd.Length - 1)
         {
-            nuvarandeOrd = ord[Random.Range(0, ord.Length)];
+            nuvarandeOrd = ord[UnityEngine.Random.Range(0, ord.Length)];
             textMesh.text = nuvarandeOrd;
             morse.text = string.Empty;
             nuvarandeOrdMorse = ConvertToMorse(nuvarandeOrd);
@@ -243,14 +288,141 @@ public class Morse : MonoBehaviour
         }
     }
 
+    private bool keyboard = true;
+
+    public void KeyboardSwitch()//Switch from keyboard to press.
+    {
+        keyboard = !keyboard;
+        timeHeldDown = 0;
+        for (int i = 0; i < KeyboardObjects.Length; i++)
+        {
+            KeyboardObjects[i].SetActive(keyboard);
+        }
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+
+    public void TurnOffBeep()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        keyboard = true;
+        timeHeldDown = 0;
+        for (int i = 0; i < KeyboardObjects.Length; i++)
+        {
+            KeyboardObjects[i].SetActive(keyboard);
+        }
+    }
+
     public void DashPressed()
     {
         morse.text += "-";
         UpdateWord();
     }
 
+    private bool heldDown = false;
+    private float timeHeldDown = 0;
+
+    private float unitLength = 0.04f;
+
     // Update is called once per frame
     private void Update()
     {
+        if (!keyboard)
+        {
+            //#if UNITY_ANDROID
+            //            if (Input.touchCount > 0)
+            //#elif UNITY_EDITOR
+            if ((Input.touchCount > 0 || Input.GetMouseButton(0)) && !heldDown)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    //timeIndex = 0;  //resets timer before playing sound
+                    audioSource.Play();
+                    //proceduralAudio.enabled = true;
+                }
+            }
+            else if ((Input.touchCount <= 0 && !Input.GetMouseButton(0)) && heldDown)
+            {
+                if (audioSource.isPlaying)
+                {
+                    audioSource.Stop();
+                }
+            }
+
+            if (Input.GetMouseButton(0) || Input.touchCount > 0)
+            //#endif
+            {
+                heldDown = true;
+                timeHeldDown += Time.deltaTime;
+
+                //if (!audioSource.isPlaying)
+                //{
+                //    //timeIndex = 0;  //resets timer before playing sound
+                //    audioSource.Play();
+                //}
+                //else
+                //{
+                //    audioSource.Stop();
+                //}
+            }
+            else if (timeHeldDown > 0)
+            {
+                if (timeHeldDown > unitLength * 3)
+                {
+                    DashPressed();
+                }
+                else
+                {
+                    DotPressed();
+                }
+                timeHeldDown = 0;
+                heldDown = false;
+            }
+        }
+    }
+
+    [Range(1, 20000)]  //Creates a slider in the inspector
+    public float frequency = 100;
+
+    public float sampleRate = 44100;
+    private AudioSource audioSource;
+
+    private float phase = 0;
+
+    //void Start()
+    //{
+    //}
+
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //    }
+    //}
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        for (int i = 0; i < data.Length; i += channels)
+        {
+            phase += 2 * Mathf.PI * frequency / sampleRate;
+
+            data[i] = Mathf.Sin(phase);
+
+            if (phase >= 2 * Mathf.PI)
+            {
+                phase -= 2 * Mathf.PI;
+            }
+        }
+    }
+
+    //Creates a sinewave
+    public float CreateSine(int timeIndex, float frequency, float sampleRate)
+    {
+        return Mathf.Sin(2 * Mathf.PI * timeIndex * frequency / sampleRate);
     }
 }
